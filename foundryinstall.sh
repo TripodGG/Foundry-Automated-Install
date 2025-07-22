b953c11c87c06432090afc4ad4bbf4fd7c420cd4
TripodGG <157990933+TripodGG@users.noreply.github.com>
2025-07-22 16:43:58 -0500

    Update foundryinstall.sh
    set a stop for pm2 to safely shut down foundry and allow deleting the individual folders and create the symlinks, then restart foundry via pm2

--- foundryinstall.sh
+++ foundryinstall.sh
@@ -548,8 +548,9 @@ echo -e ${green}"✅ PM2 configuration for $instanceName written and saved."${un
 # Symlink to the global shared assets and modules folders and confirm www-data owns the folders
 log "Creating symlink: $dataDir/Data/assets -> $assetsDir"
 log "Creating symlink: $dataDir/Data/modules -> $modulesDir"
-sudo rm -rf "$dataDir/Data/assets"
-sudo rm -rf "$dataDir/Data/modules"
+sudo -u www-data pm2 stop all
+sudo rm -R "$dataDir/Data/assets"
+sudo rm -R "$dataDir/Data/modules"
 sudo ln -sfn "$assetsDir" "$dataDir/Data/assets"
 sudo ln -sfn "$modulesDir" "$dataDir/Data/modules"
 if [ -L "$dataDir/Data/assets" ] && [ -L "$dataDir/Data/modules" ]; then
@@ -557,6 +558,7 @@ if [ -L "$dataDir/Data/assets" ] && [ -L "$dataDir/Data/modules" ]; then
 else
 	log "❌ Failed to create one or more symlinks."
 fi
+sudo -u www-data pm2 start all
 
 # ---- Finish script and close ---- #
 sudo systemctl start apache2
